187a3a88dd919323ea6ba8668efc928baaa8c2e1
Juanpnj <juan.graciano.tik22@mhsw.pnj.ac.id>
2023-07-04 11:51:39 +0700

    1aja

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>Description</h1>
-      <p>Add a description of your website here.</p>
+    <div className="m-">
+      <div className="my-auto mx-auto text-center">
+        <h1>To-Do List</h1>
+        <p>Help you stay organized, manage your tasks efficiently, and boost your productivity.</p>
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
